637aa9c7deb1dd59367a0ce6ab63c420e1567000
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2023-09-21 13:44:55 -0500

    Initial Commit

---
+++ src/main/java/com/duberlyguarnizo/bootcampgatewayservice/BootcampGatewayServiceApplication.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023. Code by Duberly Guarnizo <duberlygfr@gmail.com>.
+ */
+
+package com.duberlyguarnizo.bootcampgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class BootcampGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duberlyguarnizo/bootcampgatewayservice/BootcampGatewayServiceApplicationTests.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023. Code by Duberly Guarnizo <duberlygfr@gmail.com>.
+ */
+
+package com.duberlyguarnizo.bootcampgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
